daffffb87adbb0a7c339cd2c98c6f43aa44d43e7
CHANDRAKANT TIWARI <chandrakant91550@gmail.com>
2023-03-12 17:34:24 +0530

    DAY1

---
+++ data.js
@@ -0,0 +1,8 @@
+const data =[
+{name:'babu', home:'dto',mob:'2322'},
+{name:'bab2', home:'dto',mob:'2322'},
+{name:'bab3', home:'dto',mob:'2322'},
+{name:'bab4', home:'dtro',mob:'2322'},
+{name:'bab5', home:'dt3',mob:'2322'},
+]
+module.exports =data;
